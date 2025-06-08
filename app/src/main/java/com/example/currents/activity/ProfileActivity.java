f5ad43ab625e3cd0ea8867cedc3dce3e064ec044
Pubudu Ishan <pubudu.ishan@laugfs.lk>
2025-06-08 12:46:23 +0530

    Removed unwanted package

--- app/src/main/java/com/example/currents/activity/ProfileActivity.java
+++ app/src/main/java/com/example/currents/activity/ProfileActivity.java
@@ -15,7 +15,6 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.cardview.widget.CardView;
